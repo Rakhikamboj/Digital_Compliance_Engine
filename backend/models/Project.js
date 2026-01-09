6173516b8d6debe95f2b234a4f35ae0089b0716a
rakhikamboz7 <rakhikamboz7@gmail.com>
2026-01-09 13:11:10 +0530

    changed-status

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -13,7 +13,7 @@ const ProjectSchema = new mongoose.Schema(
     assignedAuditor: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     status: {
       type: String,
-      enum: ["Not Assigned", "Assigned", "Started", "In Progress", "Completed"],
+      enum: ["Not Assigned", "Assigned", "In Progress", "Completed"],
       default: "Not Assigned",
     },
     formData: {
